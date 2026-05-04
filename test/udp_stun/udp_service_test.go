a03c376c3efb6235daec9a37e7bcd97a91db5aef
ZluxYao <zluxyao@gmail.com>
2026-05-04 22:11:34 +0800

    udp 测试函数 定时发包

--- test/udp_stun/udp_service_test.go
+++ test/udp_stun/udp_service_test.go
@@ -14,16 +14,16 @@ import (
 //  2. 终端B: PUBLIC_ADDR=公网IP:Port go test -v -run TestUDPService
 //     (Windows PowerShell: $env:PUBLIC_ADDR='公网IP:Port'; go test -v -run TestUDPService)
 //  3. 回到终端A 看是否打印 "收到 from ..."
-func TestUDPService(t *testing.T) {
-	addr := "183.6.155.130:65442"
-	// addr := "127.0.0.1:3334"
-	if addr == "" {
-		t.Skip("跳过: 设置 PUBLIC_ADDR=公网IP:端口 后再跑 (从 main 输出复制)")
-	}
+const (
+	targetAddr   = "14.19.73.174:21741" // 从 main 输出复制
+	sendInterval = 2 * time.Second       // 发送间隔
+	testDuration = 30 * time.Second      // 持续发多久
+)
 
-	raddr, err := net.ResolveUDPAddr("udp", addr)
+func TestUDPService(t *testing.T) {
+	raddr, err := net.ResolveUDPAddr("udp", targetAddr)
 	if err != nil {
-		t.Fatalf("解析 %s: %v", addr, err)
+		t.Fatalf("解析 %s: %v", targetAddr, err)
 	}
 	conn, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
@@ -31,9 +31,26 @@ func TestUDPService(t *testing.T) {
 	}
 	defer conn.Close()
 
-	msg := fmt.Sprintf("hello stun hole @%s", time.Now().Format("15:04:05"))
-	if _, err = conn.Write([]byte(msg)); err != nil {
-		t.Fatalf("write: %v", err)
+	t.Logf("开始每 %v 发一次, 持续 %v -> %s", sendInterval, testDuration, targetAddr)
+
+	ticker := time.NewTicker(sendInterval)
+	defer ticker.Stop()
+	deadline := time.After(testDuration)
+	seq := 0
+
+	for {
+		select {
+		case <-deadline:
+			t.Logf("发送结束, 共发 %d 包", seq)
+			return
+		case ts := <-ticker.C:
+			seq++
+			msg := fmt.Sprintf("#%d hello @%s", seq, ts.Format("15:04:05"))
+			if _, err = conn.Write([]byte(msg)); err != nil {
+				t.Errorf("write #%d: %v", seq, err)
+				return
+			}
+			t.Logf("发 #%d: %s", seq, msg)
+		}
 	}
-	t.Logf("已发送 %q -> %s, 去 main 终端看是否打印 '收到 from ...'", msg, addr)
 }
